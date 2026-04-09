381ddba9ff8afb0378d0040b9aa90eb27eccf888
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-09 22:08:16 +0200

    Codeforces: 112A - Petya and Strings

---
+++ codeforces/problems/800/112A-petya-and-strings.cpp
@@ -0,0 +1,31 @@
+// https://codeforces.com/problemset/problem/112/A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string lowercase(string w) {
+  for (char &c : w)
+    c = tolower(c);
+  return w;
+}
+
+int main() {
+  int result = 0;
+  string first, second;
+  cin >> first >> second;
+
+  first = lowercase(first);
+  second = lowercase(second);
+
+  for (int i = 0; i < first.length(); i++) {
+    if (first[i] < second[i]) {
+      result = -1;
+      break;
+    } else if (first[i] > second[i]) {
+      result = 1;
+      break;
+    }
+  }
+  cout << result;
+}
