d4a2d83a42bc663a982328c82ffc859b101b5d35
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-09 19:42:21 +0200

    Codeforces: 71A - Way Too Long Words

---
+++ codeforces/problems/800/071A-way-too-long-words.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/problemset/problem/71/A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isTooLong(const string& w) {
+    return w.length() > 10;
+}
+
+string shortenWord(const string& w) {
+    char firstLetter = w[0];
+    char lastLetter = w[w.length() - 1];
+
+    return firstLetter + to_string(w.length() - 2) + lastLetter;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+
+        if(!isTooLong(word))
+            cout << word << endl;
+        else
+            cout << shortenWord(word) << endl;
+    }
+
+    return 0;
+}
