0980a2fc9c45615f9ab01bcca53cee79ee29fb3e
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-09 20:12:04 +0200

    Codeforces: 50A - Domino piling

--- codeforces/problems/800/050A-domino-piling.cpp
+++ codeforces/problems/800/050A-domino-piling.cpp
@@ -1,14 +1,11 @@
 // https://codeforces.com/problemset/problem/50/A
 
-#include <cmath>
 #include <iostream>
 using namespace std;
 
 int main() {
     int m, n;
     cin >> m >> n;
-
-    int numberOfSquares = (m * n) / 2;
-    cout << floor(numberOfSquares) << endl;
+    cout << (m * n) / 2 << endl;
     return 0;
 }
