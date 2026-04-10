a9e1319ec4cf64322b8c0a77e821d962f167f9bd
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-10 15:36:20 +0200

    Codeforces: 281A - Word Capitalization

---
+++ codeforces/problems/800/281A-word-capitalization.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/281/A
+
+#include <cctype>
+#include <iostream>
+using namespace std;
+int main() {
+    string input;
+    cin >> input;
+
+    input[0] = toupper(input[0]);
+
+    cout << input;
+    return 0;
+}
