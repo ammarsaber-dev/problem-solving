16539fc3656b3e577e3c70666d14562c276b3ebd
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-14 20:16:46 +0200

    Exercism: RNA Transcriptio

---
+++ exercism/swift/rna-transcription/Sources/RnaTranscription/RnaTranscription.swift
@@ -0,0 +1,17 @@
+func toRna(_ dna: String) -> String {
+    let dnaToRna: [Character: String] = [
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U",
+    ]
+
+    var r = ""
+    for l in dna {
+        if let m = dnaToRna[l] {
+            r.append(m)
+        }
+    }
+
+    return r
+}
