60066072a4cb641169135faf184a6687478a3956
Ammar Saber <ammarsaber.dev@icloud.com>
2026-04-14 20:07:26 +0200

    Exercism: Leap

---
+++ exercism/swift/leap/Sources/Leap/Leap.swift
@@ -0,0 +1,8 @@
+struct Year {
+    let calendarYear: Int
+
+    var isLeapYear: Bool {
+        calendarYear.isMultiple(of: 400)
+            || (calendarYear.isMultiple(of: 4) && !calendarYear.isMultiple(of: 100))
+    }
+}
